e1636f092bd47f5953c8fb95da88db110146e51e
Andrei Ionita <andrei.ionita.1902@gmail.com>
2022-09-01 15:52:42 +0300

    Add basic project stub

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let name;
+	export let age;
+
+	function onClick() {
+		age += 1
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click="{onClick}">Change age</button>

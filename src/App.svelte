351059057ee772bec819989e868c4d9483d2a2aa
Andrei Ionita <andrei.ionita.1902@gmail.com>
2023-07-13 16:26:20 +0300

    Add units 13, 14, 15

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,22 @@
 
 	$: uppercaseName = name.toUpperCase(); // Must use $ here
 
+	$: console.log(name);
+
+	$: if (name === 'Ionica') {
+		age = 20;
+	}
+
 	function onClick() {
 		age += 1
 	}
 
 	function changeName() {
 		name = 'Ionica'
+	}
+
+	function nameInput(event) {
+		name = event.target.value;
 	}
 </script>
 
@@ -22,3 +32,5 @@
 <h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click="{onClick}">Change age</button>
 <button on:click="{changeName}">Change name</button>
+<input type="text" value="{name}" on:input={nameInput}>
+<input type="text" bind:value="{name}">
